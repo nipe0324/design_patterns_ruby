26cf0df928156a90cdded14cdb76b2e9d38b52e0
nipe0324 <nipe0324@gmail.com>
2015-01-29 22:48:25 +0900

    [observer] Use Observable module of standard library

--- observer/employee.rb
+++ observer/employee.rb
@@ -1,28 +1,18 @@
+require 'observer'
+
 class Employee
+  include Observable
+
   attr_reader :name
   attr_accessor :title, :salary
 
   def initialize(name, title, salary)
     @name, @title, @salary = name, title, salary
-    @observers = []
   end
 
   def salary=(new_salary)
     @salary = new_salary
-    notify_observers
-  end
-
-  def notify_observers
-    @observers.each do |observer|
-      observer.update self
-    end
-  end
-
-  def add_observer(observer)
-    @observers << observer
-  end
-
-  def delete_observer(observer)
-    @observers.delete observer
+    changed
+    notify_observers(self)
   end
 end
