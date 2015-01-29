4afbbd60fec38ad9d5643128d7611f6b98221bc6
nipe0324 <nipe0324@gmail.com>
2015-01-29 22:39:01 +0900

    [observer] Add TaxMan Observer

--- observer/main.rb
+++ observer/main.rb
@@ -1,10 +1,14 @@
 require 'employee'
 require 'payroll'
+require 'tax_man'
 
 fred = Employee.new('Fred Flintstone', 'Crane Operator', 30_000)
 
 payroll = Payroll.new
 fred.add_observer payroll
+
+tax_man = TaxMan.new
+fred.add_observer tax_man
 
 # フレッドを昇給させる
 fred.salary = 35_000
---
+++ observer/tax_man.rb
@@ -0,0 +1,5 @@
+class TaxMan
+  def update(changed_employee)
+    puts "#{changed_employee.name}に新しい税金の請求書を送ります"
+  end
+end
