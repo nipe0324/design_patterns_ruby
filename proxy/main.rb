3d06f5fe63dc3a2d45d232d0494a6f8f411d909f
nipe0324 <nipe0324@gmail.com>
2015-02-01 16:59:06 +0900

    [proxy] add virtual proxy

--- proxy/main.rb
+++ proxy/main.rb
@@ -1,5 +1,6 @@
 require 'bank_account'
 require 'bank_account_proxy'
+require 'virtual_account_proxy'
 
 account = BankAccount.new 100
 account.deposit 50
@@ -10,3 +11,7 @@ proxy = BankAccountProxy.new account
 proxy.deposit 50
 proxy.withdraw 10
 puts proxy.balance #=> 180 (140 + 50 - 10)
+
+
+# 仮装プロキシ
+account = VirtualAccountProxy.new { BankAccount.new 10 }
