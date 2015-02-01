832d9350635f0bea78f31d932babb65a5529c3e4
nipe0324 <nipe0324@gmail.com>
2015-02-01 18:03:16 +0900

    [decorator] refactor WriterDecorator by using Forwardable

--- decorator/writer_decorator.rb
+++ decorator/writer_decorator.rb
@@ -1,21 +1,11 @@
+require 'forwardable'
+
 class WriterDecorator
+  # 委譲のためのメソッドを自動的に生成してくれる
+  extend Forwardable
+  def_delegators :@real_writer, :write_line, :pos, :rewind, :close
+
   def initialize(real_writer)
     @real_writer = real_writer
-  end
-
-  def write_line(line)
-    @real_writer.write_line line
-  end
-
-  def pos
-    @real_writer.pos
-  end
-
-  def rewind
-    @real_writer.rewind
-  end
-
-  def close
-    @real_writer.close
   end
 end
