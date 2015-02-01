3fce9484ac5ea37045f1018e0c04c0a4085a7213
nipe0324 <nipe0324@gmail.com>
2015-02-01 16:34:44 +0900

    [proxy] add proxy pattern

---
+++ proxy/main.rb
@@ -0,0 +1,12 @@
+require 'bank_account'
+require 'bank_account_proxy'
+
+account = BankAccount.new 100
+account.deposit 50
+account.withdraw 10
+puts account.balance #=> 140 (100 + 50 - 10)
+
+proxy = BankAccountProxy.new account
+proxy.deposit 50
+proxy.withdraw 10
+puts account.balance #=> 180 (140 + 50 - 10)
