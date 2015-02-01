9f3fad5d16786125795a97150e350098d6df2467
nipe0324 <nipe0324@gmail.com>
2015-02-01 18:07:45 +0900

    [decorator] add rapping method which is ruby special tech

--- decorator/main.rb
+++ decorator/main.rb
@@ -31,3 +31,21 @@ writer2.write_line 'Hello out there'
 writer2.write_line 'Good evening'
 puts "チェックサムは #{writer2.check_sum}"
 writer2.close
+
+
+# ラッピングメソッド
+writer3 = SimpleWriter.new 'rapping.txt'
+
+class << writer3
+
+  # write_lineに別名のoriginal_write_lineを作成する
+  alias original_write_line write_line
+
+  # write_lineをオーバーライドする
+  # original_write_lineは昔のwrite_lineを指している
+  def write_line(line)
+    original_write_line "#{Time.now}: #{line}"
+  end
+end
+
+writer3.write_line "タイムスタンプ付き"
