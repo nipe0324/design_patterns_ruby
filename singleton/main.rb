bb07cec5318cdef80207b6edcc1cbef4dd739054
nipe0324 <nipe0324@gmail.com>
2015-02-03 12:14:29 +0900

    [singleton] Add singleton partten

---
+++ singleton/main.rb
@@ -0,0 +1,9 @@
+require 'simple_logger'
+
+
+logger = SimpleLogger.instance
+puts logger.level
+
+SimpleLogger.instance.info('Computer wins chess game.')
+SimpleLogger.instance.warning('AE-35 hardware failure predicted.')
+SimpleLogger.instance.error('HAL-9000 malfunction, take emergency action!')
---
+++ singleton/simple_logger.rb
@@ -0,0 +1,35 @@
+class SimpleLogger
+  attr_accessor :level
+
+  ERROR = 1
+  WARNING = 2
+  INFO = 3
+
+  def initialize
+    @log = File.open('log.txt', 'w')
+    @level = WARNING
+  end
+
+  @@instance = self.new
+
+  def self.instance
+    @@instance
+  end
+
+  private_class_method :new
+
+  def error(msg)
+    @log.puts(msg)
+    @log.flush
+  end
+
+  def warning(msg)
+    @log.puts(msg) if @level >= WARNING
+    @log.flush
+  end
+
+  def info(msg)
+    @log.puts(msg) if @level >= INFO
+    @log.flush
+  end
+end
